b48117cb8d01982a1e343abb13d934fb4a66b05f
theoohoho <topslimer@gmail.com>
2021-02-24 11:55:28 +0800

    Update settings.py

--- urmart/settings.py
+++ urmart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'afccjc53dqbs5)i4^p53(pf+4ay*43ft+(*axbwl=b8&bojsg8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
